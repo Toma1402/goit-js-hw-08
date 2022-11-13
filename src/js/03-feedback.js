e4f53ece1d179ec2f01f08fa89d2b74b87af7035
Toma1402 <toma.k1402@gmail.com>
2022-11-13 11:19:15 +0000

    init

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const feedbackFormEl = document.querySelector('.feedback-form');
-const FIELDVALUES_KEY = 'feedback-form-state';
-let inputValues = JSON.parse(localStorage.getItem(FIELDVALUES_KEY)) || {};
+const STORAGE_KEY = 'feedback-form-state';
+let inputValues = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 feedbackFormEl.elements.email.value = inputValues.email || '';
 feedbackFormEl.elements.message.value = inputValues.message || '';
@@ -13,12 +13,12 @@ function onInputHandler(event) {
   } else if (event.target.nodeName === 'TEXTAREA') {
     inputValues.message = event.target.value;
   }
-  localStorage.setItem(FIELDVALUES_KEY, JSON.stringify(inputValues));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputValues));
 }
 function onFormSubmit(event) {
   event.preventDefault();
   console.log(inputValues);
-  localStorage.removeItem(FIELDVALUES_KEY);
+  localStorage.removeItem(STORAGE_KEY);
   feedbackFormEl.reset();
   inputValues = {};
 }
