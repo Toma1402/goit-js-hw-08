06dfb753291e702e56f664a5713bc41f59ff9996
Toma1402 <toma.k1402@gmail.com>
2022-11-14 16:44:53 +0000

    add 0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,4 +11,4 @@ function onTime(data) {
 
 player.on('timeupdate', throttle(onTime, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
